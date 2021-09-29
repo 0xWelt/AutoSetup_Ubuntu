260fe494c992bd9d11ec4523d53191eda153fb40
Nickydusk <dinghao12601@126.com>
2021-09-29 14:48:33 +0800

    Rename Files

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 # 执行各安装脚本
 apt/set_apt.sh
-oh-my-zsh/ohmyzsh_install.sh
-python/anaconda_install.sh      # 如果已经安装过anaconda则不会进行任何修改
+oh-my-zsh/install_ohmyzsh.sh
+python/install_python.sh      # 如果已经安装过anaconda则不会进行任何修改
+nvidia-driver/install_nvidia.sh 
 
 
 # 切换到zsh
