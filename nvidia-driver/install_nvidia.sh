db6ff3a113c52176d4b13c2b70839d1464340a7e
Nickydusk <dinghao12601@126.com>
2021-10-09 10:27:14 +0800

    modify NVIDIA

--- nvidia-driver/install_nvidia.sh
+++ nvidia-driver/install_nvidia.sh
@@ -1,4 +1,4 @@
-sudo apt install -y --upgrade python-software-properties software-properties-common ubuntu-drivers-common
+sudo apt install -y --upgrade python-software-properties build-essential gcc-multilib dkms software-properties-common ubuntu-drivers-common
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
 sudo apt-get update
 
@@ -9,11 +9,9 @@ if ubuntu-drivers devices | grep "NVIDIA Corporatio"; then
         sudo apt-get purge -y "*cublas*" "cuda*" "*nvidia*"
         sudo apt autoremove -y
 
-        # 安装最新驱动
-        sudo ubuntu-drivers autoinstall
-
         # 关闭所有当前依赖进程
         sudo rmmod nvidia_uvm
+        sudo rmmod nvidia_drm
         sudo rmmod nvidia_modeset
         while sudo lsof -n -w /dev/nvidia* | grep "COMMAND" > /dev/null
         do
@@ -25,6 +23,8 @@ if ubuntu-drivers devices | grep "NVIDIA Corporatio"; then
         sudo rmmod nvidia
 
         # 重新装在nvidia驱动
+        sudo ubuntu-drivers autoinstall   # TODO:不是最新，改成手动装470
+        
         nvidia-smi
         echo "Nvidia-Driver Installed Successfully!"
     else
