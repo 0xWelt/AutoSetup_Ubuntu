764146da386b7dd10a6de3cc853f4df49ae55d80
Hao Ding <181220010@smail.nju.edu.cn>
2022-05-02 09:25:09 +0800

    add alias

--- nvidia-driver/install_nvidia.sh
+++ nvidia-driver/install_nvidia.sh
@@ -9,28 +9,27 @@ if ubuntu-drivers devices | grep "NVIDIA Corporatio"; then
     sudo apt-get purge -y "*cublas*" "cuda*" "*nvidia*"
     sudo apt autoremove -y
 
-        # 关闭所有当前依赖进程
-        sudo rmmod nvidia_uvm
-        sudo rmmod nvidia_drm
-        sudo rmmod nvidia_modeset
-        while sudo lsof -n -w /dev/nvidia* | grep "COMMAND" > /dev/null
-        do
-            proc=$(sudo lsof -n -w /dev/nvidia* | grep -v "COMMAND" | head -n 1)
-            proc=${proc#*" "}
-            proc=${proc%%" "*}
-            sudo kill $proc
-        done
-        sudo rmmod nvidia
+    # 关闭所有当前依赖进程
+    sudo rmmod nvidia_uvm
+    sudo rmmod nvidia_drm
+    sudo rmmod nvidia_modeset
+    while sudo lsof -n -w /dev/nvidia* | grep "COMMAND" >/dev/null; do
+      proc=$(sudo lsof -n -w /dev/nvidia* | grep -v "COMMAND" | head -n 1)
+      proc=${proc#*" "}
+      proc=${proc%%" "*}
+      sudo kill $proc
+    done
+    sudo rmmod nvidia
 
-        # 重新装在nvidia驱动
-        sudo ubuntu-drivers autoinstall   # TODO:不是最新，改成手动装470
-        
-        nvidia-smi
-        echo "Nvidia-Driver Installed Successfully!"
-    else
-        nvidia-smi
-        echo "Nvidia-Driver Already Installed!"
-    fi
+    # 重新装在nvidia驱动
+    sudo ubuntu-drivers autoinstall # TODO:不是最新，改成手动装470
+
+    nvidia-smi
+    echo "Nvidia-Driver Installed Successfully!"
+  else
+    nvidia-smi
+    echo "Nvidia-Driver Already Installed!"
+  fi
 else
   echo "No Nvidia Device, Skip!"
 fi
--- oh-my-zsh/.zshrc
+++ oh-my-zsh/.zshrc
@@ -110,3 +110,9 @@ source $ZSH/oh-my-zsh.sh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+alias au="sudo apt update; sudo apt dist-upgrade -y; sudo apt autoremove -y"
+alias sr="screen -R"
+alias sls="screen -ls"
+alias rm="trasn-put"
+alias rl="trash-list"
