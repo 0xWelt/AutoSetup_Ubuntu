cc82d3d9dedcedaf1e95d595e298b9cf77558bf7
Nickydusk <dinghao12601@126.com>
2024-08-28 11:04:43 +0800

    update tmux

--- apt/set_apt.sh
+++ apt/set_apt.sh
@@ -3,30 +3,30 @@ if [ ! -d "$FILE/../tmp" ]; then
   mkdir "$FILE/../tmp"
 fi
 
-# 备份老apt源
-if [ ! -d "/etc/apt/sources.list.old" ]; then
-  sudo cp /etc/apt/sources.list /etc/apt/sources.list.old
-fi
+# # 备份老apt源
+# if [ ! -d "/etc/apt/sources.list.old" ]; then
+#   sudo cp /etc/apt/sources.list /etc/apt/sources.list.old
+# fi
 
-# 按照Ubuntu发行版，换对应的南大源
-distribution=$(cat /etc/os-release | grep -o "UBUNTU_CODENAME=.*")
-distribution=${distribution##*UBUNTU_CODENAME=}
-cat >$FILE/../tmp/sources.list <<EOF
-# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
-deb http://mirror.nju.edu.cn/ubuntu/ ${distribution} main restricted universe multiverse
-# deb-src http://mirror.nju.edu.cn/ubuntu/ ${distribution} main restricted universe multiverse
-deb http://mirror.nju.edu.cn/ubuntu/ ${distribution}-updates main restricted universe multiverse
-# deb-src http://mirror.nju.edu.cn/ubuntu/ ${distribution}-updates main restricted universe multiverse
-deb http://mirror.nju.edu.cn/ubuntu/ ${distribution}-backports main restricted universe multiverse
-# deb-src http://mirror.nju.edu.cn/ubuntu/ ${distribution}-backports main restricted universe multiverse
-deb http://mirror.nju.edu.cn/ubuntu/ ${distribution}-security main restricted universe multiverse
-# deb-src http://mirror.nju.edu.cn/ubuntu/ ${distribution}-security main restricted universe multiverse
+# # 按照Ubuntu发行版，换对应的南大源
+# distribution=$(cat /etc/os-release | grep -o "UBUNTU_CODENAME=.*")
+# distribution=${distribution##*UBUNTU_CODENAME=}
+# cat >$FILE/../tmp/sources.list <<EOF
+# # 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+# deb http://mirror.nju.edu.cn/ubuntu/ ${distribution} main restricted universe multiverse
+# # deb-src http://mirror.nju.edu.cn/ubuntu/ ${distribution} main restricted universe multiverse
+# deb http://mirror.nju.edu.cn/ubuntu/ ${distribution}-updates main restricted universe multiverse
+# # deb-src http://mirror.nju.edu.cn/ubuntu/ ${distribution}-updates main restricted universe multiverse
+# deb http://mirror.nju.edu.cn/ubuntu/ ${distribution}-backports main restricted universe multiverse
+# # deb-src http://mirror.nju.edu.cn/ubuntu/ ${distribution}-backports main restricted universe multiverse
+# deb http://mirror.nju.edu.cn/ubuntu/ ${distribution}-security main restricted universe multiverse
+# # deb-src http://mirror.nju.edu.cn/ubuntu/ ${distribution}-security main restricted universe multiverse
 
-# 预发布软件源，不建议启用
-# deb http://mirror.nju.edu.cn/ubuntu/ ${distribution}-proposed main restricted universe multiverse
-# deb-src http://mirror.nju.edu.cn/ubuntu/ ${distribution}-proposed main restricted universe multiverse
-EOF
-sudo cp $FILE/../tmp/sources.list /etc/apt/sources.list
+# # 预发布软件源，不建议启用
+# # deb http://mirror.nju.edu.cn/ubuntu/ ${distribution}-proposed main restricted universe multiverse
+# # deb-src http://mirror.nju.edu.cn/ubuntu/ ${distribution}-proposed main restricted universe multiverse
+# EOF
+# sudo cp $FILE/../tmp/sources.list /etc/apt/sources.list
 
 # 更新系统内的包，并安装所需的基础包
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
--- python/install_python.sh
+++ python/install_python.sh
@@ -4,8 +4,8 @@ if [ ! -d "$FILE/../tmp" ]; then
 fi
 
 # 下载安装anaconda
-wget -c -P $FILE/../tmp/ https://repo.anaconda.com/archive/Anaconda3-2023.03-Linux-x86_64.sh
-sh $FILE/../tmp/Anaconda3-2023.03-Linux-x86_64.sh -b
+wget -c -P $FILE/../tmp/ https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh
+sh $FILE/../tmp/Anaconda3-2024.06-1-Linux-x86_64.sh -b
 
 # 进行anaconda初始化
 user=$(whoami)
@@ -13,34 +13,34 @@ shell=$(grep $user /etc/passwd)
 shell=${shell##*/}
 ~/anaconda3/bin/conda init $shell # 即使已经装过，也再进行一次init
 
-# 换南大源
-if [ ! -d "~/.condarc.old" ]; then
-  if [ -d "~/.condarc" ]; then
-    cp ~/.condarc ~/.condarc.old
-  fi
-fi
-cat >~/.condarc <<EOF
-channels:
-  - defaults
-show_channel_urls: true
-default_channels:
-  - https://mirror.nju.edu.cn/anaconda/pkgs/main
-  - https://mirror.nju.edu.cn/anaconda/pkgs/r
-  - https://mirror.nju.edu.cn/anaconda/pkgs/msys2
-custom_channels:
-  conda-forge: https://mirror.nju.edu.cn/anaconda/cloud
-  msys2: https://mirror.nju.edu.cn/anaconda/cloud
-  bioconda: https://mirror.nju.edu.cn/anaconda/cloud
-  menpo: https://mirror.nju.edu.cn/anaconda/cloud
-  pytorch: https://mirror.nju.edu.cn/anaconda/cloud
-  simpleitk: https://mirror.nju.edu.cn/anaconda/cloud
-EOF
+# # 换南大源
+# if [ ! -d "~/.condarc.old" ]; then
+#   if [ -d "~/.condarc" ]; then
+#     cp ~/.condarc ~/.condarc.old
+#   fi
+# fi
+# cat >~/.condarc <<EOF
+# channels:
+#   - defaults
+# show_channel_urls: true
+# default_channels:
+#   - https://mirror.nju.edu.cn/anaconda/pkgs/main
+#   - https://mirror.nju.edu.cn/anaconda/pkgs/r
+#   - https://mirror.nju.edu.cn/anaconda/pkgs/msys2
+# custom_channels:
+#   conda-forge: https://mirror.nju.edu.cn/anaconda/cloud
+#   msys2: https://mirror.nju.edu.cn/anaconda/cloud
+#   bioconda: https://mirror.nju.edu.cn/anaconda/cloud
+#   menpo: https://mirror.nju.edu.cn/anaconda/cloud
+#   pytorch: https://mirror.nju.edu.cn/anaconda/cloud
+#   simpleitk: https://mirror.nju.edu.cn/anaconda/cloud
+# EOF
 
-# 更改pip为南大源
-if [ ! -d "~/.config/pip/pip.conf" ]; then
-  mkdir -vp ~/.config/pip
-fi
-cat >~/.config/pip/pip.conf <<EOF
-[global]
-index-url = https://mirror.nju.edu.cn/pypi/web/simple
-EOF
+# # 更改pip为南大源
+# if [ ! -d "~/.config/pip/pip.conf" ]; then
+#   mkdir -vp ~/.config/pip
+# fi
+# cat >~/.config/pip/pip.conf <<EOF
+# [global]
+# index-url = https://mirror.nju.edu.cn/pypi/web/simple
+# EOF
