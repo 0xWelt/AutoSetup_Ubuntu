97657e8ce718b51334d793ddaaeb46ef4bfc5958
dinghao <dinghao@msh.team>
2026-05-07 14:35:05 +0800

    fix(apt): 去掉 upgrade 和 autoremove，仅保留 update

--- apt/set_apt.sh
+++ apt/set_apt.sh
@@ -7,5 +7,5 @@ if [ ! -d "$FILE/../tmp" ]; then
 fi
 
 # 更新系统内的包，并安装所需的基础包
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+sudo apt update
 sudo apt install -y --upgrade language-pack-en zsh tmux git vim htop trash-cli wget cowsay net-tools git-lfs aria2
